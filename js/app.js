76e13afe966a4ee74ada6e380c4f908f4c98d33a
kinwail <kinwail@gmail.com>
2018-06-24 18:47:04 -1000

    completed 1 thru 7

--- js/app.js
+++ js/app.js
@@ -2,17 +2,48 @@
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
 
+var changeNumber = document.getElementById('matCount');
+//console.log(changeNumber);
+changeNumber.innerHTML ='11'
+
+// Or another method is to do below:
+//matCount.innerHTML = '12'
+
+
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
+
+msgCount.innerHTML ='23'
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
+fullname.innerHTML='Ronald McDonald'
+
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
+
+var ageChange = document.getElementById('age')
+ageChange.innerHTML ='63 Years Old'
 
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
 
+var jobBox = document.createElement('div');
+jobBox.id = 'job'
+jobBox.innerHTML = 'Clown and Restauranteur';
+
+data.appendChild(jobBox);
+
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
+var hobbiesBox = document.createElement('div');
+hobbiesBox.id = 'hobbies'
+hobbiesBox.innerHTML = 'Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.'
+data.appendChild(hobbiesBox);
+
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
+
+var locationBox = document.createElement('div');
+locationBox.id = 'location'
+locationBox.innerHTML = 'Honolulu, HI'
+data.appendChild(locationBox);
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
 
